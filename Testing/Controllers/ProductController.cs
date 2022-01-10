024453f726cec616c5ac79db47f6e54bd23cbf0b
acallahan97 <callahanamanda877@gmail.com>
2022-01-10 12:33:09 -0700

    update product

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Testing.Models;
 
 namespace Testing.Controllers
 {
@@ -25,6 +26,24 @@ namespace Testing.Controllers
 
             return View(product);
         }
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = repo.GetProduct(id);
+
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+
+            return View(prod);
+        }
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
+
 
     }
 }
